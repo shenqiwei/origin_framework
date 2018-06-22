2d437b5d94a656ba25c9a5d27f32317fae60c043
shenqiwei <cheerup.shen@foxmail.com>
2018-06-23 02:09:15 +0800

    B_plan

--- Origin/Kernel/Entrance.class.php
+++ Origin/Kernel/Entrance.class.php
@@ -21,36 +21,11 @@ class Entrance
     */
     static $_Function = null;
     /**
-     * 自动化加载路口文件
-     * @access public
-     * @return null
-     */
-    static function starting()
-    {
-        /**
-         * 使用请求器和验证结构进行入口保护
-         * @var string $_class 带命名空间信息的类信息
-         * @var string $_object 类实例化对象
-         * @var string $_method 类对象方法
-         */
-        # 根据配置路由模式，调用不同的路由解析模块
-        switch(Config('ROUTE_TYPE')){
-            case 'developer':
-                self::developer(); // 调用开发者模式
-                break;
-            case 'default':
-            default:
-                self::path(); //调用路径模式
-                break;
-        }
-        return null;
-    }
-    /**
      * 默认模式，自动加载入口
      * @access public
      * @return null
      */
-    static function path()
+    static function starting()
     {
         /**
          * 使用请求器和验证结构进行入口保护
@@ -84,84 +59,94 @@ class Entrance
             $_use = $_SERVER["REMOTE_ADDR"];
             # 调用日志结构函数
             accessLogs("[".$_protocol."] [".$_server."] [Request:".$_type."] to ".$_http.$_request.", by user IP:".$_use);
-            # 重定义指针， 起始位置0
-            $_i = 0;
-            if(!empty($_path) and !is_null($_path)){
-                # 转化路径为数组结构
-                $_path_array = explode('/',$_path);
-                # 判断首元素结构是否与默认应用目录相同
-                if($_path_array[$_i] != Config('DEFAULT_APPLICATION') and is_dir(str_replace('/', SLASH, ROOT.Config('ROOT_APPLICATION').$_path_array[0]))){
-                    # 变更应用文件夹位置
-                    $_catalogue = $_path_array[$_i].SLASH;
-                    # 指针下移
-                    $_i += 1;
-                    if($_i < count($_path_array)){
+            # 判断执行对象是否为程序单元
+            if(self::Ex($_request)) {
+                # 重定义指针， 起始位置0
+                $_i = 0;
+                if (!empty($_path) and !is_null($_path)) {
+                    # 转化路径为数组结构
+                    $_path_array = explode('/', $_path);
+                    # 判断首元素结构是否与默认应用目录相同
+                    if ($_path_array[$_i] != Config('DEFAULT_APPLICATION') and is_dir(str_replace('/', SLASH, ROOT . Config('ROOT_APPLICATION') . $_path_array[0]))) {
+                        # 变更应用文件夹位置
+                        $_catalogue = $_path_array[$_i] . SLASH;
+                        # 指针下移
+                        $_i += 1;
+                        if ($_i < count($_path_array)) {
+                            # 变更控制文件信息
+                            $_files = $_path_array[$_i];
+                            # 指针下移
+                            $_i += 1;
+                        }
+                    } else {
                         # 变更控制文件信息
                         $_files = $_path_array[$_i];
                         # 指针下移
                         $_i += 1;
                     }
-                }else{
-                    # 变更控制文件信息
-                    $_files = $_path_array[$_i];
-                    # 指针下移
-                    $_i += 1;
                 }
-            }
-            # 公共方法包引导地址
-            $_func_guide = str_replace(SLASH, ':', str_replace('/', SLASH, Config('ROOT_APPLICATION').$_catalogue.'Common/Public'));
-            # 使用钩子模型引入方法文件
-            Loading($_func_guide,Config('METHOD_SUFFIX'),'disable');
-            # 根据配置信息拼接控制器路径
-            $_path = $_catalogue.Config('APPLICATION_CONTROLLER').$_files;
-            # 设置引导地址
-            set_include_path(ROOT);
-            # 判断文件是否存在
-            if(is_file(str_replace('/', SLASH, Config('ROOT_APPLICATION').$_path.CLASS_SUFFIX))){
-                # 使用预注册加载函数，实现自动化加载
-                # 使用自动加载，实际过程中，会自动补全当前项目应用程序控制器根目录到控制器描述信息之间缺省部分
-                spl_autoload_register(function($_path){
-                    require_once(str_replace('/', SLASH, $_path.CLASS_SUFFIX));
-                });
+                # 公共方法包引导地址
+                $_func_guide = str_replace(SLASH, ':', str_replace('/', SLASH, Config('ROOT_APPLICATION') . $_catalogue . 'Common/Public'));
+                # 使用钩子模型引入方法文件
+                Loading($_func_guide, Config('METHOD_SUFFIX'), 'disable');
+                # 根据配置信息拼接控制器路径
+                $_path = $_catalogue . Config('APPLICATION_CONTROLLER') . $_files;
+                # 设置引导地址
+                set_include_path(ROOT);
+                # 判断文件是否存在
+                if (is_file(str_replace('/', SLASH, Config('ROOT_APPLICATION') . $_path . CLASS_SUFFIX))) {
+                    # 使用预注册加载函数，实现自动化加载
+                    # 使用自动加载，实际过程中，会自动补全当前项目应用程序控制器根目录到控制器描述信息之间缺省部分
+                    spl_autoload_register(function ($_path) {
+                        require_once(str_replace('/', SLASH, $_path . CLASS_SUFFIX));
+                    });
+                } else {
+                    try {
+                        throw new \Exception('Origin Method Error: Not Fount Control Document');
+                    } catch (\Exception $e) {
+                        echo($e->getMessage());
+                        exit(0);
+                    }
+                }
+                # 创建class完整信息变量
+                $_class = str_replace('/', '\\', Config('ROOT_NAMESPACE') . SLASH . $_path);
+                self::$_Class = $_class;
+                # 判断类是否存在,当自定义控制与默认控制器都不存在时，系统抛出异常
+                if (class_exists($_class)) {
+                    # 声明类对象
+                    $_object = new $_class();
+                } else {
+                    try {
+                        throw new \Exception('Origin Method Error: Not Fount Control Class');
+                    } catch (\Exception $e) {
+                        echo($e->getMessage());
+                        exit(0);
+                    }
+                }
+                # 判断是否有方法标记信息
+                if ($_path_array[$_i]) {
+                    # 如果判断标记信息，是否为控制中方法名
+                    if (method_exists($_object, $_path_array[$_i])) {
+                        $_method = $_path_array[$_i];
+                    }
+                }
+                self::$_Function = $_method;
+                # 判断方法信息是否可以被调用
+                if (method_exists($_object, $_method) and is_callable(array($_object, $_method))) {
+                    # 执行方法调用
+                    $_object->$_method();
+                } else {
+                    try {
+                        throw new \Exception('Origin Method Error: Not Fount Function Object');
+                    } catch (\Exception $e) {
+                        echo($e->getMessage());
+                        exit(0);
+                    }
+                }
             }else{
                 try {
-                    throw new \Exception('Origin Method Error: Not Fount Control Document');
-                }catch(\Exception $e){
-                    echo($e->getMessage());
-                    exit(0);
-                }
-            }
-            # 创建class完整信息变量
-            $_class = str_replace('/', '\\',Config('ROOT_NAMESPACE').SLASH.$_path);
-            self::$_Class = $_class;
-            # 判断类是否存在,当自定义控制与默认控制器都不存在时，系统抛出异常
-            if(class_exists($_class)){
-                # 声明类对象
-                $_object = new $_class();
-            }else{
-                try {
-                    throw new \Exception('Origin Method Error: Not Fount Control Class');
-                }catch(\Exception $e){
-                    echo($e->getMessage());
-                    exit(0);
-                }
-            }
-            # 判断是否有方法标记信息
-            if($_path_array[$_i]){
-                # 如果判断标记信息，是否为控制中方法名
-                if(method_exists($_object, $_path_array[$_i])){
-                    $_method = $_path_array[$_i];
-                }
-            }
-            self::$_Function = $_method;
-            # 判断方法信息是否可以被调用
-            if(method_exists($_object, $_method) and is_callable(array($_object, $_method))){
-                # 执行方法调用
-                $_object->$_method();
-            }else{
-                try {
-                    throw new \Exception('Origin Method Error: Not Fount Function Object');
-                }catch(\Exception $e){
+                    throw new \Exception('Origin Method Error: Loading Resource Object is Invalid');
+                } catch (\Exception $e) {
                     echo($e->getMessage());
                     exit(0);
                 }
@@ -170,84 +155,23 @@ class Entrance
         return null;
     }
     /**
-     * 开发模式，自动加载入口
+     * 资源类型过滤
      * @access public
-     * @return null;
+     * @param string $url 地址内容
+     * @return boolean
      */
-    static function developer()
+    static function Ex($url)
     {
-        /**
-         * 使用请求器和验证结构进行入口保护
-         * @var string $_class 带命名空间信息的类信息
-         * @var string $_object 类实例化对象
-         * @var string $_method 类对象方法
-         */
-        # 判断自动加载方法
-        if(function_exists('spl_autoload_register')){
-            # 使用预加载方法，动态加载控制器载入方法
-            /**
-             * @var string $_path 控制完整路径信息
-             */
-            # 根据配置信息拼接控制器路径
-            $_path = __APPLICATION__.Configurate('APPLICATION_CONTROLLER').SLASH.Request('GET.'.Config('RETRIEVER_CLASS'), Config('DEFAULT_CONTROLLER')).Config('SECOND_NAME');
-            # 设置引导地址
-            set_include_path(ROOT);
-            # 判断文件是否存在
-            if(is_file(str_replace('/', SLASH, Config('ROOT_APPLICATION').$_path.CLASS_SUFFIX))){
-                # 使用预注册加载函数，实现自动化加载
-                spl_autoload_register(function($_path){
-                    require_once(str_replace('/', SLASH, Config('ROOT_APPLICATION').$_path.CLASS_SUFFIX));
-                });
-            }else{
-                try {
-                    throw new \Exception('Origin Method Error: Not Fount Control Document');
-                }catch(\Exception $e){
-                    echo($e->getMessage());
-                    exit(0);
-                }
-            }
-            # 删除预设控制参数信息
-            Request('GET.'.Config('RETRIEVER_CLASS'), 'delete');
-            # 创建控制器名变量
-            $_class = str_replace('/', '\\',Config('ROOT_NAMESPACE').SLASH.$_path);
-            # 判断类是否存在,当自定义控制与默认控制器都不存在时，系统抛出异常
-            if(class_exists($_class)){
-                # 声明类对象
-                $_object = new $_class();
-            }else{
-                try {
-                    throw new \Exception('Origin Method Error: Not Fount Control Class');
-                }catch(\Exception $e){
-                    echo($e->getMessage());
-                    exit(0);
-                }
-            }
-            # 判断是否有方法标记信息
-            if(Request('GET.'.Config('RETRIEVER_METHOD'))){
-                # 如果判断标记信息，是否为控制中方法名
-                if(method_exists($_object, Request('GET.'.Config('RETRIEVER_METHOD')))){
-                    $_method = Request('GET.'.Config('RETRIEVER_METHOD'));
-                }else{
-                    $_method = Config('DEFAULT_METHOD');
-                }
-                # 删除预设方法参数信息
-                Request('GET.'.Config('RETRIEVER_METHOD'), 'delete');
-            }else{
-                $_method = Config('DEFAULT_METHOD');
-            }
-            # 判断方法信息是否可以被调用
-            if(method_exists($_object, $_method) and is_callable(array($_object, $_method))){
-                # 执行方法调用
-                $_object->$_method();
-            }else{
-                try {
-                    throw new \Exception('Origin Method Error: Not Fount Function Object');
-                }catch(\Exception $e){
-                    echo($e->getMessage());
-                    exit(0);
-                }
+        $_bool = false;
+        $_suffix = array(".html",".htm",".php");
+        for($_i = 0;$_i < count($_suffix);$_i++){
+            if(!empty(strpos($url,$_suffix[$_i]))){
+                $_bool = true;
+                break;
             }
         }
-        return null;
+        if(!$_bool)
+            if(strpos($url,".") === false) $_bool = true;
+        return $_bool;
     }
 }
