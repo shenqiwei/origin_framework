e32cd279e38f5ff013e88c19cd2ae1550d69625c
shenqiwei <cheerup.shen@foxmail.com>
2018-08-08 13:54:31 +0800

    B_plan

--- Origin/Method/Function.php
+++ Origin/Method/Function.php
@@ -86,6 +86,7 @@ Import('Export:Verify'); # 调用验证码组件
 Import('Graph:View'); # 调用界面模板匹配控制类
 # 引入路由控制函数包
 Import('Protocol:Route'); # 调用路由控制函数包
+Import('Protocol:Curl'); # 调用远程请求控制函数包
 # 应用结构包调用
 Common('Common:Public'); # 引入公共函数包
 # 公共控制器文件
