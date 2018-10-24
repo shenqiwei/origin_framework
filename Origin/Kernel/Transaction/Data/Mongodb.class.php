72264137c9b22176ccfe0d351443bff525d3af5c
shenqiwei <cheerup.shen@foxmail.com>
2018-10-24 11:23:33 +0800

    B_plan

--- Origin/Kernel/Transaction/Data/Mongodb.class.php
+++ Origin/Kernel/Transaction/Data/Mongodb.class.php
@@ -17,6 +17,26 @@ class Mongodb
     public $_Dao_connect = null;
     /**
      * @access protected
+     * @var string $_Table 表对象
+     */
+    protected $_Table = null;
+    /**
+     * @access protected
+     * @var array $_Column 字段配置
+     */
+    protected $_Column = null;
+    /**
+     * @access protected
+     * @var array $_Data 数据
+     */
+    protected $_Data = null;
+    /**
+     * @access protected
+     * @var string $_Type 语句执行类型
+     */
+    protected $_Type = "select";
+    /**
+     * @access protected
      * @var string $_Error_code 错误信息
      */
     public $_Error_code = null;
@@ -32,11 +52,76 @@ class Mongodb
                 $this->_Dao_connect = Dao();
             }else{
                 # 无效数据源
-                $this->_Error_code = "";
+                $this->_Error_code = "Dao source is not mongodb";
             }
         }else{
             $this->_Dao_connect = Mongodb($source);
         }
+    }
+    /**
+     * @access public
+     * @param string $table
+     * @context 表格
+     */
+    function setTable($table)
+    {
+        $this->_Table = $table;
+    }
+    /**
+     * @access public
+     * @param array $column
+     * @context 字段
+     */
+    function setColumn($column)
+    {
+        $this->_Column = $column;
+    }
+    /**
+     * @access public
+     * @param array $data
+     * @context 数据
+     */
+    function setData($data)
+    {
+        $this->_Data = $data;
+    }
+    /**
+     * @access public
+     * @param string $type
+     * @context 获取执行类型
+     */
+    function setType($type)
+    {
+        $this->_Table = $type;
+    }
+    /**
+     * @access public
+     * @return mixed
+     * @context 语句执行函数
+     */
+    function execute()
+    {
+        # 创建返回值变量
+        $_receipt = null;
+        # 选择数据执行状态
+        switch(trim(strtolower($this->_Type))){
+            case "insert":
+                $_receipt = $this->insert($this->_Table,$this->_Data['data']);
+                break;
+            case "update":
+                $_receipt = $this->update($this->_Table,$this->_Data['data'],$this->_Data['where']);
+                break;
+            case "delete":
+                $_receipt = $this->delete($this->_Table,$this->_Data['where']);
+                break;
+            case "count":
+                $_receipt = $this->count($this->_Table,$this->_Data['where']);
+                break;
+            default:
+                $_receipt = $this->select($this->_Table,$this->_Data['where']);
+                break;
+        }
+        return $_receipt;
     }
     /**
      * @access protected
@@ -45,7 +130,7 @@ class Mongodb
      * @return mixed
      * @context 查询操作
      */
-    public function select($table,$where)
+    protected function select($table,$where)
     {
         return $this->_Dao_connect->set($table)->where($where)->select();
     }
@@ -56,7 +141,7 @@ class Mongodb
      * @return mixed
      * @context 插入对象数据
      */
-    public function insert($table,$data)
+    protected function insert($table,$data)
     {
         return $this->_Dao_connect->set($table)->data($data)->insert();
     }
@@ -68,7 +153,7 @@ class Mongodb
      * @return mixed
      * @context 更新对象数据
      */
-    public function update($table,$data,$where)
+    protected function update($table,$data,$where)
     {
         return $this->_Dao_connect->set($table)->data($data)->where($where)->insert();
     }
@@ -79,7 +164,7 @@ class Mongodb
      * @return mixed
      * @context 删除对象数据
      */
-    public function delete($table,$where)
+    protected function delete($table,$where)
     {
         return $this->_Dao_connect->set($table)->where($where)->delete();
     }
@@ -90,7 +175,7 @@ class Mongodb
      * @return mixed
      * @context 查询总数
      */
-    public function count($table,$where)
+    protected function count($table,$where)
     {
         $_receipt = null;
         $_receipt = $this->_Dao_connect->set($table)->where($where)->select();
@@ -100,5 +185,10 @@ class Mongodb
             $_receipt = 0;
         }
         return $_receipt;
+    }
+
+    function getError()
+    {
+        return $this->_Error_code;
     }
 }
