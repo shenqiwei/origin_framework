169143c1d7e5281b6af5198a4288a5f82ddb90ca
shenqiwei <cheerup.shen@foxmail.com>
2019-01-10 13:00:55 +0800

    B_plan

--- Origin/Kernel/Entrance.class.php
+++ Origin/Kernel/Entrance.class.php
@@ -23,12 +23,12 @@ class Entrance
     /**
      * 获取对象类信息
      * @var string $_Class
-    */
+     */
     static $_Class = null;
     /**
      * 获取对象方法信息
      * @var string $_Function
-    */
+     */
     static $_Function = null;
     /**
      * 默认模式，自动加载入口
@@ -52,19 +52,17 @@ class Entrance
             # 默认控制器方法名
             $_method = Config('DEFAULT_METHOD');
             # 转换信息
-            $_path_array = array();
-            # NTS模式下PATH_INFO信息无法获取
-            $_path = (!is_null($_SERVER['PATH_INFO']))?$_SERVER['PATH_INFO']:$_SERVER['REQUEST_URI'];
-            # 获取的路径信息
-            $_path = Protocol\Route::execute($_path);
+            $_request_array = array();
             # 获取协议信息
             $_protocol = $_SERVER["SERVER_PROTOCOL"];
             # 获取服务软件信息
             $_server = $_SERVER["SERVER_SOFTWARE"];
             # 获取地址完整信息
             $_http = $_SERVER["HTTP_HOST"];
-            # 获取请求地址信息
-            $_request = $_SERVER["REQUEST_URI"];
+            # NTS模式下PATH_INFO信息无法获取
+            $_request = (!is_null($_SERVER['PATH_INFO']))?$_SERVER['PATH_INFO']:$_SERVER['REQUEST_URI'];
+            # 获取的路径信息
+            $_request = Protocol\Route::execute($_request);
             # 获取请求器类型
             $_type = $_SERVER["REQUEST_METHOD"];
             # 获取用户ip
@@ -75,27 +73,27 @@ class Entrance
             if(strpos($_request,'?'))
                 $_request = substr($_request,0,strpos($_request,'?'));
             # 判断执行对象是否为程序单元
-            if(self::Ex($_request)) {
+            if(self::Ex($_request)){
                 # 重定义指针， 起始位置0
                 $_i = 0;
-                if (!empty($_path) and !is_null($_path)) {
+                if (!empty($_request) and !is_null($_request)) {
                     # 转化路径为数组结构
-                    $_path_array = explode('/', $_path);
+                    $_request_array = explode('/', $_request);
                     # 判断首元素结构是否与默认应用目录相同
-                    if ($_path_array[$_i] != Config('DEFAULT_APPLICATION') and is_dir(str_replace('/', SLASH, ROOT . Config('ROOT_APPLICATION') . $_path_array[0]))) {
+                    if ($_request_array[$_i] != Config('DEFAULT_APPLICATION') and is_dir(str_replace('/', SLASH, ROOT . Config('ROOT_APPLICATION') . $_request_array[0]))) {
                         # 变更应用文件夹位置
-                        $_catalogue = $_path_array[$_i] . SLASH;
+                        $_catalogue = $_request_array[$_i] . SLASH;
                         # 指针下移
                         $_i += 1;
-                        if ($_i < count($_path_array)) {
+                        if ($_i < count($_request_array)) {
                             # 变更控制文件信息
-                            $_files = $_path_array[$_i];
+                            $_files = $_request_array[$_i];
                             # 指针下移
                             $_i += 1;
                         }
                     } else {
                         # 变更控制文件信息
-                        $_files = $_path_array[$_i];
+                        $_files = $_request_array[$_i];
                         # 指针下移
                         $_i += 1;
                     }
@@ -105,15 +103,15 @@ class Entrance
                 # 使用钩子模型引入方法文件
                 Loading($_func_guide, Config('METHOD_SUFFIX'), 'disable');
                 # 根据配置信息拼接控制器路径
-                $_path = $_catalogue . Config('APPLICATION_CONTROLLER') . $_files;
+                $_request = $_catalogue . Config('APPLICATION_CONTROLLER') . $_files;
                 # 设置引导地址
                 set_include_path(ROOT);
                 # 判断文件是否存在
-                if (is_file(str_replace('\'',SLASH,str_replace('/', SLASH, Config('ROOT_APPLICATION') . $_path . CLASS_SUFFIX)))) {
+                if (is_file(str_replace('\'',SLASH,str_replace('/', SLASH, Config('ROOT_APPLICATION') . $_request . CLASS_SUFFIX)))) {
                     # 使用预注册加载函数，实现自动化加载
                     # 使用自动加载，实际过程中，会自动补全当前项目应用程序控制器根目录到控制器描述信息之间缺省部分
-                    spl_autoload_register(function ($_path) {
-                        require_once(str_replace('\'',SLASH,str_replace('/', SLASH, $_path . CLASS_SUFFIX)));
+                    spl_autoload_register(function ($_request) {
+                        require_once(str_replace('\'',SLASH,str_replace('/', SLASH, $_request . CLASS_SUFFIX)));
                     });
                 } else {
                     try {
@@ -123,8 +121,9 @@ class Entrance
                         exit(0);
                     }
                 }
+
                 # 创建class完整信息变量
-                $_class = str_replace('/', '\\', Config('ROOT_NAMESPACE') . SLASH . $_path);
+                $_class = str_replace('/', '\\', Config('ROOT_NAMESPACE') . SLASH . $_request);
                 self::$_Class = $_class;
                 # 判断类是否存在,当自定义控制与默认控制器都不存在时，系统抛出异常
                 if (class_exists($_class)) {
@@ -139,10 +138,10 @@ class Entrance
                     }
                 }
                 # 判断是否有方法标记信息
-                if ($_path_array[$_i]) {
+                if ($_request_array[$_i]) {
                     # 如果判断标记信息，是否为控制中方法名
-                    if (method_exists($_object, $_path_array[$_i])) {
-                        $_method = $_path_array[$_i];
+                    if (method_exists($_object, $_request_array[$_i])) {
+                        $_method = $_request_array[$_i];
                     }
                 }
                 self::$_Function = $_method;
@@ -179,7 +178,7 @@ class Entrance
     {
         $_bool = false;
         if(is_bool(Config("USE_DEFAULT_ACCESS")) and !Config("USE_DEFAULT_ACCESS")){
-            $_suffix = array(".html",".htm",".php");
+            $_suffix = array(".html",".htm",".php",".png");
             for($_i = 0;$_i < count($_suffix);$_i++){
                 if(!empty(strpos($url,$_suffix[$_i]))){
                     $_bool = true;
