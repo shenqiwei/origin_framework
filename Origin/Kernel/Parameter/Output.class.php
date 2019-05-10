85107c73e2bce857f5a171636f86e95d643d4b30
shenqiwei <cheerup.shen@foxmail.com>
2019-05-10 13:05:06 +0800

    Origin class context update

--- Origin/Kernel/Parameter/Output.class.php
+++ Origin/Kernel/Parameter/Output.class.php
@@ -1,9 +1,17 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: DELL
- * Date: 2018/6/25
- * Time: 11:48
+ * coding: utf-8 *
+ * system OS: windows2008 *
+ * work Tools:Phpstorm *
+ * language Ver: php7.1 *
+ * agreement: PSR-1 to PSR-11 *
+ * filename: IoC.Origin.Kernel.Parameter.Output *
+ * version: 1.0 *
+ * @author 沈起葳 <cheerup.shen@foxmail.com>
+ * @version 0.1
+ * @since 0.1
+ * @copyright 2015-2018
+ * @context: IoC 模板管理
  */
 namespace Origin\Kernel\Parameter;
 # 文件内容输出
