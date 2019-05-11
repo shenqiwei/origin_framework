299a7a525628ab212fd7cc11698226b423aecfb5
shenqiwei <cheerup.shen@foxmail.com>
2019-05-11 23:50:42 +0800

    Origin function update

--- Origin/Kernel/Protocol/Curl.class.php
+++ Origin/Kernel/Protocol/Curl.class.php
@@ -18,22 +18,32 @@ namespace Origin\Kernel\Protocol;
 class Curl
 {
     /**
-     * @access private
+     * @access protected
      * @var array $_curl_receipt
      * @contact 请求返回信息
-    */
-    private $_curl_receipt = array();
+     */
+    protected $_curl_receipt = array();
+    /**
+     * @access protected
+     * @var boolean $_curl_utf_8
+     * @context 是否执行utf-8转码
+     */
+    protected $_curl_utf_8 = false;
     # 构造方法
-    function __construct()
-    {}
+    function __construct($bool=false)
+    {
+        $this->_curl_utf_8 = boolval($bool);
+    }
     /**
      * @access public
      * @param string $url 访问地址
      * @param string/array 访问参数，可以使用get参数结构或者（k/v）数组结构
+     * @param boolean $ssl_peer 验证证书
+     * @param boolean $ssl_host 验证地址
      * @return mixed
      * @content get请求函数
-    */
-    function get($url=null,$param=null)
+     */
+    function get($url=null,$param=null,$ssl_peer=false,$ssl_host=false)
     {
         $_receipt = null;
         if(!is_null($url)){
@@ -41,10 +51,14 @@ class Curl
             curl_setopt($_curl,CURLOPT_URL,$url);
             curl_setopt($_curl,CURLOPT_POST,false);
             curl_setopt($_curl,CURLOPT_RETURNTRANSFER,true);
-            curl_setopt($_curl,CURLOPT_SSL_VERIFYPEER,false);
-            curl_setopt($_curl,CURLOPT_SSL_VERIFYHOST,false);
-            curl_setopt($_curl,CURLOPT_POSTFIELDS,$param);
+            curl_setopt($_curl,CURLOPT_SSL_VERIFYPEER,boolval($ssl_peer));
+            curl_setopt($_curl,CURLOPT_SSL_VERIFYHOST,boolval($ssl_host));
+            if(!is_null($param))
+                curl_setopt($_curl,CURLOPT_POSTFIELDS,$param);
             $_receipt = curl_exec($_curl);
+            if($this->_curl_utf_8)
+                # 将会输内容强制转化为utf-8
+                $_receipt = mb_convert_encoding($_receipt, 'UTF-8', 'UTF-8,GBK,GB2312,BIG5');
             $this->_curl_receipt['errno'] = curl_errno($_curl);
             $this->_curl_receipt['error'] = curl_error($_curl);
             curl_close($_curl);
@@ -56,10 +70,12 @@ class Curl
      * @param string $url 访问地址
      * @param string/array 访问参数，（k/v）数组结构
      * @param string/int $type 请求值类型 0：from 表单请求，1：json json字符串请求，2：xml xml文本标记请求
+     * @param boolean $ssl_peer 验证证书
+     * @param boolean $ssl_host 验证地址
      * @return mixed
      * @content get请求函数
      */
-    function post($url,$param,$type='from')
+    function post($url,$param,$type='from',$ssl_peer=false,$ssl_host=false)
     {
         $_receipt = null;
         if(!is_null($url)){
@@ -68,8 +84,8 @@ class Curl
             curl_setopt($_curl,CURLOPT_RETURNTRANSFER,true);
             curl_setopt($_curl,CURLOPT_HEADER,false);
             curl_setopt($_curl,CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);
-            curl_setopt($_curl,CURLOPT_SSL_VERIFYPEER,false);
-            curl_setopt($_curl,CURLOPT_SSL_VERIFYHOST,false);
+            curl_setopt($_curl,CURLOPT_SSL_VERIFYPEER,boolval($ssl_peer));
+            curl_setopt($_curl,CURLOPT_SSL_VERIFYHOST,boolval($ssl_host));
             curl_setopt($_curl,CURLOPT_POST,true);
             if($type !== 'from' or (is_numeric($type) and $type !== 0)){
                 if($type === 'json' or (is_numeric($type) and $type === 1)){
@@ -93,6 +109,9 @@ class Curl
             curl_setopt($_curl,CURLOPT_TIMEOUT,30);
             curl_setopt($_curl,CURLOPT_POSTFIELDS,$param);
             $_receipt = curl_exec($_curl);
+            if($this->_curl_utf_8)
+                # 将会输内容强制转化为utf-8
+                $_receipt = mb_convert_encoding($_receipt, 'UTF-8', 'UTF-8,GBK,GB2312,BIG5');
             $this->_curl_receipt['errno'] = curl_errno($_curl);
             $this->_curl_receipt['error'] = curl_error($_curl);
             curl_close($_curl);
--- Origin/Method/Common/Public.func.php
+++ Origin/Method/Common/Public.func.php
@@ -229,72 +229,70 @@ function J($guide, $throws = 'enable')
  * @access public
  * @param array $page 分页数组
  * @param array $style 分页样式
- * @param array $search 搜索条件
+ * @param string $search 搜索条件
  * @param string $cols 页码数量
  * @return array
  * @contact 比较逻辑运算符双向转化方法
  */
-function Number($page, $style, $search, $cols)
-{
+function Number($page,$style,$search,$cols){
     //执行数字页码
-    $n = array();
-    if ($page['count'] > $cols) {
-        $k = ($cols % 2 == 0) ? $cols / 2 : ($cols - 1) / 2;
-        if (($page['current'] - $k) > 1 && ($page['current'] + $k) < $page['count']) {
-            $page['num_begin'] = $page['current'] - $k;
-            $page['num_end'] = $page['current'] + $k;
-            for ($i = $page['num_begin']; $i <= $page['num_end']; $i++) {
-                if ($i == $page['current']) {
-                    array_push($n, array('page' => $i, 'class' => $style['mouse_on'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
-                } else {
-                    array_push($n, array('page' => $i, 'class' => $style['mouse_off'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
+    $n=array();
+    if($page['count']>$cols){
+        $k=($cols%2==0)?$cols/2:($cols-1)/2;
+        if(($page['current']-$k)>1 && ($page['current']+$k)<$page['count']){
+            $page['num_begin']=$page['current']-$k;
+            $page['num_end']=$page['current']+$k;
+            for($i=$page['num_begin'];$i<=$page['num_end'];$i++){
+                if($i==$page['current']){
+                    array_push($n,array('page'=>$i,'class'=>$style['mouse_on'],'url'=>$page['url'].'?page='.$i.$search));
+                }else{
+                    array_push($n,array('page'=>$i,'class'=>$style['mouse_off'],'url'=>$page['url'].'?page='.$i.$search));
                 }
             }
-        } else {
-            if (($page['current'] - $k) <= 1) {
-                $page['num_begin'] = 1;
-                $page['num_end'] = $cols;
-                for ($i = $page['num_begin']; $i <= $page['num_end']; $i++) {
-                    if ($i == $page['current']) {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_on'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
-                    } else {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_off'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
+        }else{
+            if(($page['current']-$k)<=1){
+                $page['num_begin']=1;
+                $page['num_end']=$cols;
+                for($i=$page['num_begin'];$i<=$page['num_end'];$i++){
+                    if($i==$page['current']){
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_on'],'url'=>$page['url'].'?page='.$i.$search));
+                    }else{
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_off'],'url'=>$page['url'].'?page='.$i.$search));
                     }
                 }
-            } elseif (($page['current'] + $k) >= $page['count']) {
-                $page['num_begin'] = $page['count'] - ($cols - 1);
-                $page['num_end'] = $page['count'];
-                for ($i = $page['num_begin']; $i <= $page['num_end']; $i++) {
-                    if ($i == $page['current']) {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_on'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
-                    } else {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_off'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
+            }elseif(($page['current']+$k)>=$page['count']){
+                $page['num_begin']=$page['count']-($cols-1);
+                $page['num_end']=$page['count'];
+                for($i=$page['num_begin'];$i<=$page['num_end'];$i++){
+                    if($i==$page['current']){
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_on'],'url'=>$page['url'].'?page='.$i.$search));
+                    }else{
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_off'],'url'=>$page['url'].'?page='.$i.$search));
                     }
                 }
-            } else {
-                $page['num_begin'] = 1;
-                $page['num_end'] = $cols;
-                for ($i = $page['num_begin']; $i <= $page['num_end']; $i++) {
-                    if ($i == $page['current']) {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_on'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
-                    } else {
-                        array_push($n, array('page' => $i, 'class' => $style['mouse_off'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
+            }else{
+                $page['num_begin']=1;
+                $page['num_end']=$cols;
+                for($i=$page['num_begin'];$i<=$page['num_end'];$i++){
+                    if($i==$page['current']){
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_on'],'url'=>$page['url'].'?page='.$i.$search));
+                    }else{
+                        array_push($n,array('page'=>$i,'class'=>$style['mouse_off'],'url'=>$page['url'].'?page='.$i.$search));
                     }
                 }
             }
         }
-    } else {
-        for ($i = 1; $i <= $page['count']; $i++) {
-            if ($i == $page['current']) {
-                array_push($n, array('page' => $i, 'class' => $style['mouse_on'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
-            } else {
-                array_push($n, array('page' => $i, 'class' => $style['mouse_off'], 'url' => $page['url'] . '?page=' . $i . $search['search_page']));
+    }else{
+        for($i=1;$i<=$page['count'];$i++){
+            if($i==$page['current']){
+                array_push($n,array('page'=>$i,'class'=>$style['mouse_on'],'url'=>$page['url'].'?page='.$i.$search));
+            }else{
+                array_push($n,array('page'=>$i,'class'=>$style['mouse_off'],'url'=>$page['url'].'?page='.$i.$search));
             }
         }
     }
     return $n;
 }
-
 /**
  * @access public
  * @param string $url 链接
@@ -302,51 +300,50 @@ function Number($page, $style, $search, $cols)
  * @param string $current 当前页数
  * @param array $style 分页样式
  * @param string $row 分页大小
- * @param array $search 搜索条件
+ * @param string $search 搜索条件
  * @return array
  * @contact 比较逻辑运算符双向转化方法
  */
-function Page($url, $count, $current, $style, $row, $search)
-{
-    $page = array(
-        'url' => $url,
-        'size' => intval($row), 'num_begin' => 0, 'num_end' => 0, 'count' => 0, 'limit' => 0, 'current' => 1,//翻页基本参数
-        'first_class' => $style['first'], 'first_url' => '', 'first' => 0,//第一页参数
-        'last_class' => $style['previous'], 'last_url' => '', 'last' => 0,//上一页参数
-        'next_class' => $style['next'], 'next_url' => '', 'next' => 0,//下一页参数
-        'end_class' => $style['last'], 'end_url' => '', 'end' => 0,//最后一页参数
-        'num' => 5,//页码翻页参数
+function Page($url,$count,$current,$style,$row,$search){
+    $page=array(
+        'url'=>$url,
+        'size'=>intval($row),'num_begin'=>0,'num_end'=>0,'count'=>0,'limit'=>0,'current'=>1,//翻页基本参数
+        'first_class'=>$style['first'],'first_url'=>'','first'=>0,//第一页参数
+        'last_class'=>$style['previous'],'last_url'=>'','last'=>0,//上一页参数
+        'next_class'=>$style['next'],'next_url'=>'','next'=>0,//下一页参数
+        'end_class'=>$style['last'],'end_url'=>'','end'=>0,//最后一页参数
+        'num'=>5,//页码翻页参数
     );
-    $page['current'] = intval($current);
-    $page['count'] = $count % $page['size'] != 0 ? intval(($count / $page['size']) + 1) : intval($count / $page['size']);
+    $page['current']=intval($current);
+    $page['count']=$count%$page['size']!=0?intval(($count/$page['size'])+1):intval($count/$page['size']);
     //判断页标状态
-    if ($page['current'] <= 0) $page['current'] = 1;
-    if ($page['current'] > $page['count']) $page['current'] = $page['count'];
-    if ($page['count'] <= 0) $page['current'] = $page['count'] = 1;
-    $page['limit'] = $page['size'] * ($page['current'] - 1);//其实点运算
-    $page['page_one'] = $page['limit'] + 1;
-    $page['page_end'] = ($page['limit'] + $page['size']) > $count ? $count : $page['limit'] + $page['size'];
+    if($page['current']<=0) $page['current']=1;
+    if($page['current']>$page['count']) $page['current']=$page['count'];
+    if($page['count']<=0) $page['current']=$page['count']=1;
+    $page['limit']=$page['size']*($page['current']-1);//其实点运算
+    $page['page_one']=$page['limit']+1;
+    $page['page_end']=($page['limit']+$page['size'])>$count?$count:$page['limit']+$page['size'];
     //判断翻页状态1
-    if ($page['current'] > 1) {
-        $page['last'] = $page['current'] - 1;
-    } else {
-        $page['last'] = 1;
-        $page['first_class'] .= $style['mouse_off'];
-        $page['last_class'] .= $style['mouse_off'];
+    if($page['current']>1){
+        $page['last']=$page['current']-1;
+    }else{
+        $page['last']=1;
+        $page['first_class'].=$style['mouse_off'];
+        $page['last_class'].=$style['mouse_off'];
     }
 
     //判断翻页状态2
-    if ($page['current'] >= $page['count']) {
-        $page['next'] = $page['count'];
-        $page['next_class'] .= $style['mouse_off'];
-        $page['end_class'] .= $style['mouse_off'];
-    } else {
-        $page['next'] = $page['current'] + 1;
+    if($page['current']>=$page['count']){
+        $page['next']=$page['count'];
+        $page['next_class'].=$style['mouse_off'];
+        $page['end_class'].=$style['mouse_off'];
+    }else{
+        $page['next']=$page['current']+1;
     }
-    $page['first_url'] = $page['url'] . '?page=1' . $search['search_page'];//第一页
-    $page['last_url'] = $page['url'] . '?page=' . $page['last'] . $search['search_page'];//上一页
-    $page['next_url'] = $page['url'] . '?page=' . $page['next'] . $search['search_page'];//下一页
-    $page['end_url'] = $page['url'] . '?page=' . $page['count'] . $search['search_page'];//最后一页
+    $page['first_url']=$page['url'].'?page=1'.$search;//第一页
+    $page['last_url']=$page['url'].'?page='.$page['last'].$search;//上一页
+    $page['next_url']=$page['url'].'?page='.$page['next'].$search;//下一页
+    $page['end_url']=$page['url'].'?page='.$page['count'].$search;//最后一页
     return $page;
 }
 
