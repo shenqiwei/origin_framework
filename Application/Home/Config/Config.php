01b039e15c0f14f8a86a06a47c283e1a745cdef1
shenqiwei <cheerup.shen@foxmail.com>
2019-11-18 01:44:14 +0800

    Origin framework md file update

--- Application/Home/Config/Config.php
+++ Application/Home/Config/Config.php
@@ -1,20 +1,6 @@
 <?php
 /**
- * coding: utf-8 *
- * system OS: windows2008 *
- * work Tools:Phpstorm *
- * language Ver: php7.1 *
- * agreement: PSR-1 to PSR-11 *
- * filename: IoC.Apply[.Home].Config.Config *
- * version: 1.0 *
- * structure: common framework *
- * designer: 沈启威 *
- * developer: 沈启威 *
- * partner: 沈启威 *
- * chinese Context:
- * create Time: 2017/01/01 9:35
- * update Time: 2017/01/12 10:45
- * IoC 框架配置文件
+ * @context (Default Visit) Application configuration folder
  */
 return array(
 );
