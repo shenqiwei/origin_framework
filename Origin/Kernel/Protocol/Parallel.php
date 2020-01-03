4d13b7b73593482c1ca9731e19e745926436e3ed
shenqiwei <cheerup.shen@foxmail.com>
2020-01-03 14:17:55 +0800

    Origin framework update

---
+++ Origin/Kernel/Protocol/Parallel.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author 沈起葳 <cheerup.shen@foxmail.com>
+ * @version 0.1
+ * @copyright 2015-2017
+ * @context: Origin框架多线程
+ */
+namespace Origin\Kernel\Protocol;
+
+use parallel\Runtime;
+use parallel\Channel;
+use parallel\Future;
+use parallel\Events;
+use parallel\Events\Input;
+use parallel\Events\Event;
+use parallel\Events\Event\Type;
+use parallel\Sync;
+
+class Parallel
+{
+    function __construct()
+    {}
+
+    function main()
+    {
+    }
+}
