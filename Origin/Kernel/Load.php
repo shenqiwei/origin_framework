c6d1d8306e6b554c0175e5855a0b2068f4086e62
shenqiwei <cheerup.shen@foxmail.com>
2020-01-14 11:49:31 +0800

    Origin framework update

--- Origin/Kernel/Load.php
+++ Origin/Kernel/Load.php
@@ -126,7 +126,7 @@ class Load
                     }else{
                         $_404 = str_replace("/",DS,ROOT.Config("ROOT_RESOURCE")."/Public/Temp/404.html");
                         if(is_file($_404)){
-                            echo("404");
+                            echo("ERROR:404");
                             exit();
                         }else{
                             include($_404);
