b249761f9f46cf3e30dcbcfa2d89d4d70620f4a8
shenqiwei <cheerup.shen@foxmail.com>
2020-10-24 13:10:32 +0800

    update origin framework <folder package>

---
+++ origin/package/Folder.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * @author 沈起葳 <cheerup.shen@foxmail.com>
+ * @version 1.0
+ * @copyright 2015-2020
+ * @context: Origin框架文件夹操作封装
+ */
+namespace Origin\Package;
+
+use Exception;
+
+class Folder
+{
+    /**
+     * 根目录
+     * @var string $Root
+    */
+    protected $Root = ROOT;
+    /**
+     * @access public
+     * @param string $root 根目录地址
+     * @context 构造方法
+    */
+    function __construct($root=null)
+    {
+        if(!is_null($root))
+            $this->Root = $root;
+    }
+    /**
+     * 断点变量
+     * @var string $Breakpoint
+     */
+    protected $Breakpoint = null;
+    /**
+     * @access public
+     * @return string
+     * @context 断点信息返回
+     */
+    function getBreakpoint()
+    {
+        return $this->Breakpoint;
+    }
+    /**
+     * 错误信息
+     * @var string $Error
+     */
+    protected $Error = null;
+    /**
+     * @access public
+     * @return string
+     * @context 获取错误信息
+     */
+    function getError(){
+        return $this->Error;
+    }
+    /**
+     * @access public
+     * @param string $folder 文件夹地址
+     * @param boolean $autocomplete 自动补全完整路径，默认值 false
+     * @param boolean $throw 捕捉异常
+     * @return boolean
+     * @context 创建文件夹
+    */
+    function create($folder, $autocomplete=false,$throw=false)
+    {
+        # 设置返回对象
+        $_receipt = false;
+        # 判断文件夹是否已创建完成
+        if(file_exists($_folder = replace(ROOT.DS.$folder)))
+            $_receipt = true;
+        else{
+            if(!mkdir($_folder, 0777)){
+                $_folder = explode('/',$folder);
+                $_guide = null;
+                for($_i = 0;$_i < count($_folder);$_i++){
+                    if(empty($_i))
+                        $_guide = $_folder[$_i];
+                    else
+                        $_guide .= DS.$_folder[$_i];
+                    if(is_dir(ROOT.DS.$_guide))
+                        continue;
+                    else{
+                        if($autocomplete){
+                            if(!mkdir(ROOT.DS.$_guide,0777))
+                                $this->Breakpoint = $_folder[$_i];
+                        }else
+                            $this->Breakpoint = $_folder[$_i];
+                    }
+                }
+            }else
+                $_receipt = true;
+            if(!$_receipt){
+                # 错误代码：00101，错误信息：文件创建失败
+                $this->Error = "Create folder [{$_folder}] failed";
+                try {
+                    throw new Exception($this->Error);
+                } catch (Exception $e) {
+                    exception("Folder Error", $e->getMessage(), debug_backtrace(0, 1));
+                    exit();
+                }
+            }
+        }
+        return $_receipt;
+    }
+    /**
+     * @access public
+     * @param string $folder 文件夹地址
+     * @param boolean $throw 捕捉异常
+     * @return boolean
+     * @context 删除文件夹
+    */
+    function remove($folder,$throw=false){
+        # 设置返回对象
+        $_receipt = false;
+        if(!file_exists($_folder = replace(ROOT.DS.$folder)))
+            $_receipt = true;
+        else{
+            if(!rmdir($_folder)) {
+                $this->Error = "Remove folder [{$folder}] failed";
+                if(!$throw){
+                    try {
+                        throw new Exception($this->Error);
+                    } catch (Exception $e) {
+                        exception("Folder Error", $e->getMessage(), debug_backtrace(0, 1));
+                        exit();
+                    }
+                }
+            }else
+                $_receipt = true;
+        }
+        return $_receipt;
+    }
+    /**
+     * @access public
+     * @param string $folder 文件地址
+     * @param string $name 新名称
+     * @param boolean $throw 捕捉异常
+     * @return boolean
+     * @context 文件夹重命名
+    */
+    function rename($folder, $name, $throw=false)
+    {
+        # 设置返回对象
+        $_receipt = false;
+        if(file_exists($_folder = replace(ROOT.DS.$folder))){
+            if (!rename($_folder, $name)) {
+                # 错误代码：00102，错误信息：文件夹重命名失败
+                $this->Error = "Folder [{$_folder}] rename failed";
+                if(!$throw){
+                    try {
+                        throw new Exception($this->Error);
+                    } catch (Exception $e) {
+                        exception("Folder Error", $e->getMessage(), debug_backtrace(0, 1));
+                        exit();
+                    }
+                }
+            }
+        }else{
+            $this->Error = "The folder is invalid!";
+            if(!$throw){
+                try{
+                    throw new Exception($this->Error);
+                }catch(Exception $e){
+                    exception("Folder Error",$e->getMessage(),debug_backtrace(0,1));
+                    exit();
+                }
+            }
+        }
+        return $_receipt;
+    }
+}
